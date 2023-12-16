297409862242d6eabd2c6c1c2b9eb6d10b2937e8
lgriffithl <lliill.education.lliill@gmail.com>
2023-12-16 21:06:14 +0100

    update data controller

--- api/controllers/data.py
+++ api/controllers/data.py
@@ -5,8 +5,13 @@ from typing import Annotated
 from datetime import datetime
 
 router = APIRouter(prefix="/data",tags=["Data"])
-@router.get("/usernames")
+@router.get("/user")
 async def get_all_usernames():
-    usernames = list(db.user.find({},{'username':1,'_id':0}))
-    print(usernames)
-    return usernames
+    users = list(db.user.find({},{'username':1,'perms':1,'_id':0}))
+    for user in users:
+        if user['perms'] > 4 :
+            user['user_type'] = 'doctor';
+        else:
+            user['user_type'] = 'patient'
+        user.pop('perms')
+    return users
